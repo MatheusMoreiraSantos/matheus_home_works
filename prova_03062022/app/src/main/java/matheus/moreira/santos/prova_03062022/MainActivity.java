326d4c41fee2e2a036ee762a58e05d3a20ada58b
MatheusMoreiraSantos <106314356+MatheusMoreiraSantos@users.noreply.github.com>
2022-06-05 22:44:57 -0300

    Validação
    Implementados os códigos/métodos que validam informações.

--- prova_03062022/app/src/main/java/matheus/moreira/santos/prova_03062022/MainActivity.java
+++ prova_03062022/app/src/main/java/matheus/moreira/santos/prova_03062022/MainActivity.java
@@ -31,10 +31,12 @@ public class MainActivity extends AppCompatActivity {
     imbCheck            = findViewById(R.id.imbCheck);
     imbClear            = findViewById(R.id.imbClear);
 
+
     ActionBar actionBar = getSupportActionBar();
     if(actionBar != null){
         actionBar.setTitle("Desenvolvedores em TI");
     }
+
 
     prova01 = new ProvaQuestoesRespostas();
 
@@ -46,37 +48,53 @@ public class MainActivity extends AppCompatActivity {
                 int  pergunta = Integer . valueOf ( edtNumeroQuestao . getText () . toString ());
 
             Toast.makeText(MainActivity.this, "Por favor, insira o número da questão!", Toast.LENGTH_SHORT).show();
+        }
 
-        }else {
-                
-
-
+            int info02 = 2;
+            if(Integer.parseInt(edtNumeroQuestao.getText().toString()) == info02){
+                tvPergunta.setText(prova01.getQuestao02());
+                tvResposta.setText(prova01.getResposta02());
             }
 
+            int info03 = 3;
+            if(Integer.parseInt(edtNumeroQuestao.getText().toString()) == info03){
+                tvPergunta.setText(prova01.getQuestao03());
+                tvResposta.setText(prova01.getResposta03());
+            }
 
+            int info04 = 4;
+            if(Integer.parseInt(edtNumeroQuestao.getText().toString()) == info04){
+                tvPergunta.setText(prova01.getQuestao04());
+                tvResposta.setText(prova01.getResposta04());
+            }
 
+            int info05 = 5;
+            if(Integer.parseInt(edtNumeroQuestao.getText().toString()) == info05){
+                tvPergunta.setText(prova01.getQuestao05());
+                tvResposta.setText(prova01.getResposta05());
+            }
 
+            int info06 = 6;
+            if(Integer.parseInt(edtNumeroQuestao.getText().toString()) == info06){
+                tvPergunta.setText(prova01.getQuestao06());
+                tvResposta.setText(prova01.getResposta06());
+            }
 
+            if(Integer.parseInt(edtNumeroQuestao.getText().toString()) != info02 + info03 + info04 + info05 + info06){
+                Toast.makeText(MainActivity.this, "Por favor, insira um número válido!", Toast.LENGTH_SHORT).show();
+            }
 
         }
     });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    imbClear.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            edtNumeroQuestao.setText("");
+            tvPergunta.setText("");
+            tvResposta.setText("");
+        }
+    });
 
     }
 }
